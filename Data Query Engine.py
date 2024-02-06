e6a0b3ae885fd113ec258d0d08fd7f5d6e7b532c
sheikhjaved <sonu.javed@gmail.com>
2024-02-06 17:15:53 +1100

    Start 0.0 [Project Athena] : Athena was the goddess of reason, wisdom, and war. She famously sprung fully formed from the forehead of Zeus.

---
+++ Data Query Engine.py
@@ -0,0 +1,29 @@
+# -------------------------------------------------------------------------------
+# Description : This Script is written to query data from the Vector DB
+# Developer : Drivyx Technologies
+# Libraries used : Lama Index, Pandas,
+# Script Name : Data Query Engine.py
+# Date:  30/01/2024
+# Version : 1
+# -------------------------------------------------------------------------------
+
+# Calling all the libraries
+
+# Retrieving User query
+
+# Search/Reading matching documents from the VectorBD
+# Context 1
+
+# Search the wikipedia
+# Context 2
+
+# Search the Internet
+# Context 3
+
+# Calling LLM
+
+# Storing the output to database + Question + Context + Answer
+
+# Output Streamlit chat
+
+# Exit Gracefully
